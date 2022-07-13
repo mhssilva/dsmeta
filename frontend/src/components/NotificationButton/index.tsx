c316fda9d6a8f50313d8c70c3f4cf308dc52f107
Matheus Souza <matheus.henrique.ss@hotmail.com>
2022-07-12 23:21:52 -0300

    First component

---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,13 @@
+import iconButton from '../../assets/img/notification-icon.svg'
+import './styles.css'
+
+function NotificationButton() {
+    return(
+
+        <div className="dsmeta-red-btn">
+        <img src={iconButton} alt="Notificar" />
+      </div>
+    )
+  }
+  
+  export default NotificationButton
